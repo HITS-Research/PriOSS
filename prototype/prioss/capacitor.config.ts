c34d6a210e2af583243902acc65a40350fe5d54e
Simon Gorissen <scg@mail.upb.de>
2023-04-13 11:28:08 +0200

    Added capacitor to the project to allow building with sqlite3 support

---
+++ prototype/prioss/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.pg.prioss',
+  appName: 'Prioss',
+  webDir: 'dist/prioss',
+  bundledWebRuntime: false
+};
+
+export default config;
