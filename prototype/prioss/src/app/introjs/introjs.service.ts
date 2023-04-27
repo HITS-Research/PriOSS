c72fda706711f0d0941c17bb00d50a37b00aa78f
Sven F <svenf@mail.upb.de>
2023-04-27 10:11:39 +0200

    fix/tourstart-introjs: added generic first card as afterwards all elements are loaded correctly

--- prototype/prioss/src/app/introjs/introjs.service.ts
+++ prototype/prioss/src/app/introjs/introjs.service.ts
@@ -30,6 +30,10 @@ export class IntrojsService {
     this.introJS.setOptions({
       steps: [
         {
+          element: 'step0',
+          intro: 'New here? Click <b>Next</b>! <br><br> Already comfortable? <br> <b>Click</b> into the dashboard!'
+        },
+        {
           element: '#step1',
           intro: 'This is the first step!'
         },
@@ -70,6 +74,10 @@ export class IntrojsService {
     this.introJSF = introJs();
     this.introJSF.setOptions({
       steps: [
+        {
+          element: 'step0',
+          intro: 'New here? Click <b>Next</b>! <br><br> Already comfortable? <br> <b>Click</b> into the dashboard!'
+        },
         {
           element: '#fStep10',
           intro: 'Your Personal Data'
