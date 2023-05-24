2a0e6a13a7fd4fc3026901adcc2809721476d9e3
Ugur Yerli <ugurye@mail.uni-paderborn.de>
2023-05-24 17:33:30 +0200

    direct link added to service selection page to navigate to the download page and facebook progress bar is working now.

--- prototype/prioss/src/app/service-selection/service-selection.component.ts
+++ prototype/prioss/src/app/service-selection/service-selection.component.ts
@@ -1002,7 +1002,13 @@ export class ServiceSelectionComponent {
                 gender: personal_data.gender.gender_option
               }).subscribe((key) => {
               });
+              setTimeout(() => {
+                //TODO: properly wait for data to be available in DB
+                this.progressBarPercent = 30;
+              }, 1000);
+         
           }
+       
           else if (filename == "ads_information/advertisers_using_your_activity_or_information.json") {
             console.log('Parsing: ' + filename);
             let jsonData = JSON.parse(content);
@@ -1016,6 +1022,11 @@ export class ServiceSelectionComponent {
                 }).subscribe((key) => {
                 });
             });
+            setTimeout(() => {
+              //TODO: properly wait for data to be available in DB
+              this.progressBarPercent = 60;
+            }, 1500);
+         
           }
           else if (filename == "ads_information/advertisers_you've_interacted_with.json") {
             console.log('Parsing: ' + filename);
@@ -1153,6 +1164,10 @@ export class ServiceSelectionComponent {
                 });
             });
           }
+          setTimeout(() => {
+        
+            this.progressBarPercent = 100;
+          }, 2000);
         });
       });
       return true;
