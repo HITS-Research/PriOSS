7f0c641c8af30d40cb95f1cea884a26fc06a5ecf
maxy <maxy@mail.upb.de>
2023-05-30 02:14:34 +0200

    judge: allowed html in HowToCheck string, added information for spotify, added friendly robot icon

--- prototype/prioss/src/app/visualizations/all/privacy-settings-judge/spot-privacy-settings.service.ts
+++ prototype/prioss/src/app/visualizations/all/privacy-settings-judge/spot-privacy-settings.service.ts
@@ -6,19 +6,19 @@ import { Injectable } from '@angular/core';
 export class SpotPrivacySettingsService {
   settings = [
     {
-      Question : "Public Profile?",
-      HowToCheck : "Go to your Spotify settings ... 1",
+      Question : "Are you getting tailored advertisments ?",
+      HowToCheck : "<strong>Text Guide:</strong> Go to <a target='_blank' href='https://Spotify.com'>Spotify.com</a> &#8594 On the top-right press on the small icon of a person &#8594 click on 'Account' &#8594 Privacy settings <br> <br> <strong>Picture Guide:</strong> -- Link Here --",
       Options : [
-        { label: "Option 23", value: "option23", advice: "No Idea if its Good" },
-        { label: "Option 22", value: "option22", advice: "change it" }
+        { label: "Yes", value: "yes", advice: "No Idea if its Good" },
+        { label: "No", value: "no", advice: "change it" }
                 ]
     },
     {
-      Question : "Sharing User Data with Spotify ?",
-      HowToCheck : "Go to your Spotify settings ... 2",
+      Question : "Is your Facebook data linked to your acount?",
+      HowToCheck : "<strong>Text Guide:</strong> Go to <a target='_blank' href='https://Spotify.com'>Spotify.com</a> &#8594 On the top-right press on the small icon of a person &#8594 click on 'Account' &#8594 Privacy settings <br> <br> <strong>Picture Guide:</strong> -- Link Here --",
       Options : [
-        { label: "Option 99", value: "option99", advice: "welp now google knows it too" },
-        { label: "Option 98", value: "option98", advice: "Doesnt really matter" }
+        { label: "Yes", value: "yes", advice: "welp now google knows it too" },
+        { label: "No", value: "no", advice: "Doesnt really matter" }
       ]
     } ]
 
