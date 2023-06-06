2e93454ba401f40d634424879efcb3bf2c104882
Melina Kleber <kleber@mail.uni-paderborn.de>
2023-06-06 06:47:29 +0200

    graph event handler works

--- prototype/prioss/src/app/visualizations/instagram/insta-followers/insta-followers.component.ts
+++ prototype/prioss/src/app/visualizations/instagram/insta-followers/insta-followers.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import cytoscape from 'cytoscape';
-import * as utilities from 'src/app/utilities/generalUtilities.functions'
+import * as utilities from 'src/app/utilities/generalUtilities.functions';
 import { InstaFollowerInfo } from 'src/app/models/Instagram/FollowerInfo/FollowerInfo';
 import { InstaFollowingInfo } from 'src/app/models/Instagram/FollowerInfo/FollowingInfo';
 import { InstaBlockedInfo } from 'src/app/models/Instagram/FollowerInfo/BlockedInfo';
@@ -12,38 +12,48 @@ import { InstaBlockedRepository } from 'src/app/db/data-repositories/instagram/i
   selector: 'app-insta-followers',
   templateUrl: './insta-followers.component.html',
   styleUrls: ['./insta-followers.component.less'],
-  providers: [InstaFollowerRepository, InstaFollowingRepository, InstaBlockedRepository]
+  providers: [
+    InstaFollowerRepository,
+    InstaFollowingRepository,
+    InstaBlockedRepository,
+  ],
 })
-
-
 export class InstaFollowersComponent {
   @Input()
   previewMode: boolean = false;
 
-  followerInfo : InstaFollowerInfo[] = [];
-  followingInfo : InstaFollowingInfo[] = [];
-  blockedInfo : InstaBlockedInfo[] = [];
-  graphElements: { data: { id?: string, source?: string, target?: string  }, position?: {x: number, y: number} }[] = [];
+  followerInfo: InstaFollowerInfo[] = [];
+  followingInfo: InstaFollowingInfo[] = [];
+  blockedInfo: InstaBlockedInfo[] = [];
+  graphElements: {
+    data: { id?: string; source?: string; target?: string };
+  }[] = [];
 
   getObjectPairs: (obj: object) => [string, any][] = utilities.getObjectPairs;
   convertTimestamp: (str: string) => any = utilities.convertTimestamp;
-  capitalizeAndPrettify: (str: string) => string = utilities.capitalizeAndPrettify;
+  capitalizeAndPrettify: (str: string) => string =
+    utilities.capitalizeAndPrettify;
 
   graphOptions = ['Followers', 'Following'];
   selectedTags: string[] = ['Followers', 'Following'];
 
-   // Default page configuration for Follower / Following
-   currentFollowerPage = 1;
-   currentFollowingPage = 1;
-   currentBlockedPage = 1;
-   pageSize = 10;
+  // Default page configuration for Follower / Following
+  currentFollowerPage = 1;
+  currentFollowingPage = 1;
+  currentBlockedPage = 1;
+  pageSize = 10;
 
-  constructor(private instaFollowerRepo: InstaFollowerRepository, private instaFollowingRepo: InstaFollowingRepository, private instaBlockedRepo: InstaBlockedRepository) {
-  }
+  cy: cytoscape.Core;
 
-   /**
+  constructor(
+    private instaFollowerRepo: InstaFollowerRepository,
+    private instaFollowingRepo: InstaFollowingRepository,
+    private instaBlockedRepo: InstaBlockedRepository
+  ) {}
+
+  /**
    * Stores all needed data from the different tables into the corresponding interface variables.
-   * 
+   *
    * @author: Melina (kleber@mail.uni-paderborn.de)
    */
   async collectData() {
@@ -63,118 +73,117 @@ export class InstaFollowersComponent {
 
   /**
    * Prepare the follower/following information for the graph representation. Means changing the format of the representation of this information.
-   * 
+   *
    * @author: Melina (kleber@mail.uni-paderborn.de)
    */
-  prepareGraphData(){
+  prepareGraphData() {
     //Creates a set of nodes for the star-pattern
     const nodes = new Set<string>();
-    if(this.selectedTags.includes('Followers')){
-      this.followerInfo.forEach(follower => nodes.add(follower.instaAccountName)); 
-    } 
-    if(this.selectedTags.includes('Following')){
-      this.followingInfo.forEach(following => nodes.add(following.instaAccountName)); 
-    } 
+    if (this.selectedTags.includes('Followers')) {
+      this.followerInfo.forEach((follower) =>
+        nodes.add(follower.instaAccountName)
+      );
+    }
+    if (this.selectedTags.includes('Following')) {
+      this.followingInfo.forEach((following) =>
+        nodes.add(following.instaAccountName)
+      );
+    }
     const numberOfNodes = nodes.size;
     //Gives every node their position in the cycle and add the nodes to the graphElement array
     this.graphElements = [...nodes].map((node, index) => ({
       data: {
         id: node,
-      },
-      position: { 
-        x: 100 + 200 * Math.cos(360/numberOfNodes*index),
-        y: 100 + 200 * Math.sin(360/numberOfNodes*index),
-      },
-
+      }
     }));
 
     //adds the users node to the array and fix his position into the center
     const you = 'you';
     this.graphElements.push({
-      data:{
+      data: {
         id: you,
-      },
-      position:{
-        x: 100,
-        y: 100,
       }
     });
     //Add the edges between the user to its followers
-    this.followerInfo.forEach((follower)=>{
-      this.graphElements.push({
-        data:{
-          //id: `${follower.instaAccountName}${you}`,
-          source: follower.instaAccountName,
-          target: you,
-        }
+    if (this.selectedTags.includes('Followers')) {
+      this.followerInfo.forEach((follower) => {
+        this.graphElements.push({
+          data: {
+            source: follower.instaAccountName,
+            target: you,
+          },
+        });
       });
-    });
+    }
     //Add the edges between the user to its following users
-    this.followingInfo.forEach((following)=>{
-      this.graphElements.push({
-        data:{
-          //id: `${following.instaAccountName}${you}`,
-          source: you,
-          target: following.instaAccountName,
-        }
+    if (this.selectedTags.includes('Following')) {
+      this.followingInfo.forEach((following) => {
+        this.graphElements.push({
+          data: {
+            source: you,
+            target: following.instaAccountName,
+          },
+        });
       });
-    });
+    }
+  }
+
+  /**
+   * Update the graphElements and apply the layout again
+   *
+   * @author: Melina (kleber@mail.uni-paderborn.de)
+   */
+  updateGraph() {
+    this.prepareGraphData();
+    this.cy.elements().remove();
+    this.cy.add(this.graphElements);
+    this.cy.layout(({name:'circle', padding:30, fit:true})).run();
   }
 
   /**
    * Builds the graph for the followers and following accounts.
-   * 
+   *
    * @author: Melina (kleber@mail.uni-paderborn.de)
    */
   async ngAfterViewInit() {
     await this.collectData();
     this.prepareGraphData();
-    var cy = cytoscape({
+    this.cy = cytoscape({
       container: document.getElementById('cy'), // container to render in
-      elements: this.graphElements,     
-      style: [ // the stylesheet for the graph
+      elements: this.graphElements,
+      style: [
+        // the stylesheet for the graph
         {
           selector: 'node',
           style: {
             'background-color': '#666',
-            'label': 'data(id)'
-          }
+            label: 'data(id)',
+          },
         },
         {
           selector: 'edge',
           style: {
-            'width': 3,
+            width: 3,
             'line-color': '#ccc',
             'target-arrow-color': '#ccc',
             'target-arrow-shape': 'triangle',
             'curve-style': 'bezier',
-          }
-        }
+          },
+        },
       ],
       layout: {
-        //name: 'preset',
-        //name: 'random',
         name: 'circle',
         padding: 30,
-        fit: true,
-      }
-    
+        fit: true
+      },
     });
   }
 
-  handleChange(checked: boolean, tag: string): void {
-    if (checked) {
-      this.selectedTags.push(tag);
-    } else {
-      this.selectedTags = this.selectedTags.filter(t => t !== tag);
-    }
-    this.prepareGraphData();
-    console.log('You are interested in: ', this.selectedTags);
-  }
+  //Getter
 
   /**
    * This method slice a dataset for the tables in 10 entrys per page
-   * 
+   *
    * @param data the dataset that sould be sliced
    * @param currentPage the current page of the dataset
    * @returns the sliced data
@@ -189,14 +198,16 @@ export class InstaFollowersComponent {
   get sliced_follower_data() {
     return this.getSlicedData(this.followerInfo, this.currentFollowerPage);
   }
-  
+
   get sliced_following_data() {
     return this.getSlicedData(this.followingInfo, this.currentFollowingPage);
   }
-  
+
   get sliced_blocked_data() {
     return this.getSlicedData(this.blockedInfo, this.currentBlockedPage);
   }
+
+  //Event Handler
 
   // Changing the page number based on user selection
   on_follower_page_change(event: any) {
@@ -212,8 +223,17 @@ export class InstaFollowersComponent {
   on_blocked_page_change(event: any) {
     this.currentBlockedPage = event;
   }
+
+  /**
+   * Updates the selected Tags that are used for filtering what graph elements are used
+   *
+   */
+  handleChange(checked: boolean, tag: string): void {
+    if (checked) {
+      this.selectedTags.push(tag);
+    } else {
+      this.selectedTags = this.selectedTags.filter((t) => t !== tag);
+    }
+    this.updateGraph();
+  }
 }
-
-
-
-
