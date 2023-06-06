1b519ddad53f2877fede33528965883098111c2b
Sven F <svenf@mail.upb.de>
2023-06-06 10:16:54 +0200

    offline-indication: add comments

--- prototype/prioss/src/app/offline-indicator/offline-indicator.component.ts
+++ prototype/prioss/src/app/offline-indicator/offline-indicator.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+/**
+ * Component that displays a wlan icon.
+ * Changes its color depending on the connectivity.
+ * 
+ * @author: Sven (svenf@mail.upb.de)
+ */
 @Component({
   selector: 'app-offline-indicator',
   templateUrl: './offline-indicator.component.html',
@@ -8,6 +15,11 @@ import { Component, OnInit } from '@angular/core';
 export class OfflineIndicatorComponent implements OnInit {
   onlineStatus: boolean;
 
+  /**
+   * This method calls @updateOnlineStatus() on init and adds eventlisteners to wlan icon.
+   * 
+   * @author: Sven (svenf@mail.upb.de)
+   */
   ngOnInit() {
     this.updateOnlineStatus();
 
@@ -15,6 +27,12 @@ export class OfflineIndicatorComponent implements OnInit {
     window.addEventListener('offline', this.updateOnlineStatus.bind(this));
   }
 
+  /**
+   * This method changes the online status based on the connectivity. 
+   * https://developer.mozilla.org/en-US/docs/Web/API/Navigator/onLine
+   * 
+   * @author: Sven (svenf@mail.upb.de)
+   */
   updateOnlineStatus() {
     this.onlineStatus = navigator.onLine;
   };
