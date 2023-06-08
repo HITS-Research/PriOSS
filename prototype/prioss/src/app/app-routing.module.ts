86f9adbd985e5d8df7c3807de0a142eed4559c48
Durva Ghurye <dghurye@campus.uni-paderborn.de>
2023-06-08 17:20:33 +0200

    Add FAQ related questions to it's dedicated page
    Add FAQ related questions to it's dedicated page

--- prototype/prioss/src/app/app-routing.module.ts
+++ prototype/prioss/src/app/app-routing.module.ts
@@ -24,12 +24,14 @@ import { FriendAndFollowersComponent } from './visualizations/facebook/friend-an
 import { OffFacebookActivityComponent } from './rectification/facebook/off-facebook-activity/off-facebook-activity.component';
 import { InstaFollowersComponent } from './visualizations/instagram/insta-followers/insta-followers.component';
 import { InstaContactComponent } from './visualizations/instagram/insta-contact/insta-contact.component';
+import { FaqComponent } from './info-pages/faq/faq.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: LandingComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'faq', component: FaqComponent },
   { path: 'spot/inference', component: InferencesComponent },
   { path: 'spot/mood', component: MoodComponent },
   { path: 'spot/listening-time', component: ListeningTimeComponent },
