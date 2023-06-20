aacf7eb15048923c7098a12246d6a904d7197222
Jonathan <jvn@mail.uni-paderborn.de>
2023-06-20 18:34:43 +0200

    spot-privacy-instr: added text guides

--- prototype/prioss/src/app/visualizations/all/privacy-settings-judge/spot-privacy-settings.service.ts
+++ prototype/prioss/src/app/visualizations/all/privacy-settings-judge/spot-privacy-settings.service.ts
@@ -16,31 +16,29 @@ export class SpotPrivacySettingsService {
   settings = [
     {
       question : "Are you getting tailored advertisments?",
-      howToCheck : "<strong>Text Guide:</strong> Go to <a target='_blank' href='https://Spotify.com'>Spotify.com</a> &#8594 On the top-right press on the small icon of a person &#8594 click on 'Account' &#8594 Privacy settings <br> <br> <strong>Picture Guide:</strong> -- Link Here --",
-      options : [
+      howToCheck : "<strong>Text Guide:</strong> Login to the <a target='_blank' href='https://accounts.spotify.com/en/login'>Spotify</a> website &#8594 Choose Privacy settings &#8594 See if the option to process your personal data for tailored ads is enabled. <br> <br> <strong>Picture Guide:</strong> Please look <a target='_blank' href='/spot/privacy-instructions'>here</a> for detailed instructions.",        options : [
         { label: "Yes", value: "yes", advice: "Based on what Spotify and its advertising partners think might be interesting to you or apply to you, you receive tailored ads. It is usually not known on what basis these interests or attributes are assigned. In our dashboard you can see the list of inferences Spotify uses for tailored ads. But the meaning of these inferences is not clear and no further explanation is provided by Spotify. Consider turning this option off." },
         { label: "No", value: "no", advice: "Privacy-wise, this is a good choice. The ads you receive are not based on non-transparent profiling." }
                 ]
     },
     {
       question : "Is your Facebook data linked to your account?",
-      howToCheck : "",
-      options : [
+      howToCheck : "<strong>Text Guide:</strong> Login to the <a target='_blank' href='https://accounts.spotify.com/en/login'>Spotify</a> website &#8594 Choose Privacy settings &#8594 See if the option to process of your Facebook data is enabled. <br> <br> <strong>Picture Guide:</strong> Please look <a target='_blank' href='/spot/privacy-instructions'>here</a> for detailed instructions.",          options : [
         { label: "Yes", value: "yes", advice: "This option allows you to find Facebook friends on Spotify and vice versa. However, it is not clear what other data is shared. We recommend deactivating this option." },
         { label: "No", value: "no", advice: "Very good." }
       ]
     },
     {
       question : "Do you share your registration data for marketing purposes?",
-      howToCheck : "",
+      howToCheck : "<strong>Text Guide:</strong> Login to the <a target='_blank' href='https://accounts.spotify.com/en/login'>Spotify</a> website &#8594 Choose Edit profile &#8594 See if the option to share your registration data is enabled. <br> <br> <strong>Picture Guide:</strong> Please look <a target='_blank' href='/spot/privacy-instructions'>here</a> for detailed instructions.",
       options : [
         { label: "Yes", value: "yes", advice: "Since it is not known what data exactly is shared with whom, we recommend deactivating this option." },
         { label: "No", value: "no", advice: "A good choice." }
       ]
     },
     {
-      question : "Can other people see your followers and the ones you follow?",
-      howToCheck : "",
+      question : "Can other people see your followers and the people you follow?",
+      howToCheck : "<strong>Text Guide:</strong> Open your Spotify app &#8594 Click on the icon in the top right corner and choose settings &#8594 Scroll down to the Social section &#8594 See if the option to share your follower lists is enabled. <br> <br> <strong>Picture Guide:</strong> Please look <a target='_blank' href='/spot/privacy-instructions'>here</a> for detailed instructions.",
       options : [
         { label: "Yes", value: "yes", advice: "Everyone can see who you follow and who follows you. This allows everyone to see who you might be connected to. Consider turning this option off." },
         { label: "No", value: "no", advice: "Great. Not everyone needs to know who you are connected to." }
@@ -48,7 +46,7 @@ export class SpotPrivacySettingsService {
     },
     {
       question : "Do you share what songs you are listening to with your followers?",
-      howToCheck : "",
+      howToCheck : "<strong>Text Guide:</strong> Open your Spotify app &#8594 Click on the icon in the top right corner and choose settings &#8594 Scroll down to the Social section &#8594 See if the option to share your listening activity is enabled. <br> <br> <strong>Picture Guide:</strong> Please look <a target='_blank' href='/spot/privacy-instructions'>here</a> for detailed instructions.",
       options : [
         { label: "Yes", value: "yes", advice: "The songs you listen to can give hints about your mood. With some songs, you may not want other people to know that you have heard them. Note that although this will only be shared with your followers, anyone can follow you without your prior permission. Consider turning this option off." },
         { label: "No", value: "no", advice: "Great. The songs you listen to are not publicly visible." }
@@ -56,7 +54,7 @@ export class SpotPrivacySettingsService {
     },
     {
       question : "Can other people see your recently played artists?",
-      howToCheck : "",
+      howToCheck : "<strong>Text Guide:</strong> Open your Spotify app &#8594 Click on the icon in the top right corner and choose settings &#8594 Scroll down to the Social section &#8594 See if the option to share your recently played artists is enabled. <br> <br> <strong>Picture Guide:</strong> Please look <a target='_blank' href='/spot/privacy-instructions'>here</a> for detailed instructions.",
       options : [
         { label: "Yes", value: "yes", advice: "You may do not want that everyone can see the artists you listen to. Consider turning this option off." },
         { label: "No", value: "no", advice: "Great." }
@@ -64,7 +62,7 @@ export class SpotPrivacySettingsService {
     },
     {
       question : "Are some of your playlists public?",
-      howToCheck : "",
+      howToCheck : "<strong>Text Guide:</strong> Open your Spotify app &#8594 Click on the icon in the top right corner and choose profile &#8594 See if any of your playlists are public. <br> <br> <strong>Picture Guide:</strong> Please look <a target='_blank' href='/spot/privacy-instructions'>here</a> for detailed instructions.",
       options : [
         { label: "Yes", value: "yes", advice: "If you have intentionally shared your playlists, this is not a problem. Consider checking if all your public playlists should remain public. You can select for each playlist whether it should be public." },
         { label: "No", value: "no", advice: "Good." }
