a408898ddc079e92f9907670dce3cd15b1247cef
Simon Gorissen <scg@mail.upb.de>
2023-06-26 16:02:56 +0200

    songtimeline: Added missing comment to spot history repo

--- prototype/prioss/src/app/db/data-repositories/spotify/spot-history/spot-history.repository.ts
+++ prototype/prioss/src/app/db/data-repositories/spotify/spot-history/spot-history.repository.ts
@@ -225,6 +225,12 @@ export class SpotHistoryRepository extends BulkAddCapableRepository{
     });
   }
 
+  /**
+   * Queries the spotify listening history for the information of songs listened to within on specific hour
+   * @returns An array of SpotHistoryBySong
+   *
+   * @author: Simon (scg@mail.upb.de)
+   */
   async getHistoryForSingleHour(startHour: Date): Promise<SpotHistoryBySong[]>
   {
     return this.dbService.executeQuery<any>(async (db: SQLiteDBConnection) => {
