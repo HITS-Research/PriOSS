3671cccd9516b47f822590ddd5a72c5becdb6fb5
dbelvi <dbelvi@mail.uni-paderborn.de>
2023-07-10 15:05:58 +0200

    Add new security-login-data component

---
+++ prototype/prioss/src/app/visualizations/facebook/security-login-data/security-login-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-login-data',
+  templateUrl: './security-login-data.component.html',
+  styleUrls: ['./security-login-data.component.less']
+})
+export class SecurityLoginDataComponent {
+
+}
