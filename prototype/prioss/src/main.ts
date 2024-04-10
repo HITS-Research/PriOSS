db57be7b50862aa887e0253240ec7df40f088607
sial <sial@mail.upb.de>
2024-04-10 13:44:38 +0200

    last pieces of sqlite removed

--- prototype/prioss/src/main.ts
+++ prototype/prioss/src/main.ts
@@ -2,28 +2,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import { defineCustomElements as jeepSqlite} from 'jeep-sqlite/loader';
-import { Capacitor } from '@capacitor/core';
-import { CapacitorSQLite, SQLiteConnection } from '@capacitor-community/sqlite';
 
 // Add the following import for service worker
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 
-jeepSqlite(window);
 window.addEventListener('DOMContentLoaded', async () => {
-  const platform = Capacitor.getPlatform();
-  const sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite)
   try {
-    if(platform === "web") {
-      const jeepEl = document.createElement("jeep-sqlite");
-      document.body.appendChild(jeepEl);
-      jeepEl.autoSave = true;
-      await customElements.whenDefined('jeep-sqlite');
-      await sqlite.initWebStore();
-    }
-    await sqlite.checkConnectionsConsistency();
-
     if (environment.production) {
       enableProdMode();
       if ('serviceWorker' in navigator) {
