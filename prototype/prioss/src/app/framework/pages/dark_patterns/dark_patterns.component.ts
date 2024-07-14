6c10d26511e9180bbe8133ae8650395d73754072
Subhadeep Debnath <sdebnath@mail.uni-paderborn.de>
2024-07-15 01:38:32 +0200

    Facebook - Purpose table fix - Yorick's Suggestions

--- prototype/prioss/src/app/framework/pages/dark_patterns/dark_patterns.component.ts
+++ prototype/prioss/src/app/framework/pages/dark_patterns/dark_patterns.component.ts
@@ -1,60 +1,68 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { Step } from 'src/app/features/stepper/step.type';
-import { StepperComponent } from 'src/app/features/stepper/stepper.component';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
+import { NzCarouselModule, NzCarouselComponent } from 'ng-zorro-antd/carousel';
+import { CommonModule } from '@angular/common';
 
-/**
- * This component is responsible for providing guidelines to avoid dark patterns in web applications.
- */
+interface Step {
+  title: string;
+  imageUrl: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-dark-patterns',
   templateUrl: './dark_patterns.component.html',
   styleUrls: ['./dark_patterns.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [StepperComponent, NzIconModule],
+  imports: [NzCarouselModule, CommonModule],
 })
 export class DarkPatternsComponent {
+  @ViewChild(NzCarouselComponent, { static: false }) carousel!: NzCarouselComponent;
 
-  // Initialize currentStep to track the current step
   initialIndex: number = 0;
 
-  // Steps data to be passed to the stepper component
-  #steps: Step[] = [
+  steps: Step[] = [
+    {
+      title: 'Sneak Into Basket',
+      imageUrl: '../assets/images/dark_patterns/DP1.png',
+      description: 'Websites use a sneaky tactic to add items or additional costs to your shopping cart without you realizing it.',
+    },
+    {
+      title: 'Hidden Information',
+      imageUrl: '../assets/images/dark_patterns/DP2.png',
+      description: 'Important information and options are hidden or displayed in such a way that they are hardly noticeable to obtain your consent for increased data access.',
+    },
+    {
+      title: 'Activity Notifications',
+      imageUrl: '../assets/images/dark_patterns/DP3.png',
+      description: 'Websites notify you with urgent messages such as "Offer ends in 5 minutes," creating a sense of pressure and scarcity to manipulate decision making.',
+    },
+    {
+      title: 'Preselection',
+      imageUrl: '../assets/images/dark_patterns/DP4.png',
+      description: 'Some options in a webpage or a pop-up will be pre-selected or ticked to take more advantage of you and earn profit.',
+    },
+    {
+      title: 'Forced Continuity',
+      imageUrl: '../assets/images/dark_patterns/DP5.png',
+      description: 'Some websites make it hard for you to cancel subscriptions in order to change your decision.',
+    },
     {
       title: 'No Reject Button',
-      imageUrl: '../assets/images/dark_patterns/DP1.png',
-      description: 'A banner without a <strong>reject</strong> button makes it harder for users to opt-out of tracking, nudging them to accept cookies without a clear way to say no.',
-    },
-    {
-      title: 'Pre-ticked Boxes',
-      imageUrl: '../assets/images/dark_patterns/DP2.png',
-      description: 'Automatically choose an option for you, often leading you to agree to something like receiving promotional emails without explicitly deciding to do so.',
-    },
-    {
-      title: 'Difficult to Reject',
-      imageUrl: '../assets/images/dark_patterns/DP3.png',
-      description: 'Instead of a clear <strong>no</strong> option, users are directed to another landing page, making it more difficult to decline unwanted tracking or consent.',
-    },
-    {
-      title: 'Deceptive Button',
-      imageUrl: '../assets/images/dark_patterns/DP4.png',
-      description: 'Using button colors to trick users, like making the <strong>accept</strong> button bright and eye-catching while the "decline" button is dull and less noticeable.',
-    },
-    {
-      title: 'Legitimate Interest',
-      imageUrl: '../assets/images/dark_patterns/DP5.png',
-      description: 'Claiming a right to use your data without clear consent by saying it is for a legitimate business reason, often bypassing your privacy preferences.',
-    },
-    {
-      title: 'Necessary Cookies',
       imageUrl: '../assets/images/dark_patterns/DP6.png',
-      description: 'Labeling marketing cookies, which track your online activity, as <strong>necessary</strong> implying they are needed for the website to function, even though they serve as promotions.',
+      description: 'Websites do not give you an option to reject agreements, subscriptions or policies by not providing it to have more data access about you.',
     }
   ];
 
-  get steps(): Step[] {
-    return this.#steps;
+  goToPrevious(): void {
+    if (this.carousel) {
+      this.carousel.pre();
+    }
   }
 
+  goToNext(): void {
+    if (this.carousel) {
+      this.carousel.next();
+    }
+  }
 }
